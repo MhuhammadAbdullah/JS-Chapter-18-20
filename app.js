9a311ffc93d5a2d66f851bf351f88fb1ebc5334f
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-09-15 12:03:27 +0500

    updated

---
+++ app.js
@@ -0,0 +1,48 @@
+// Question: 01
+// Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+// var multiDimensional = [[], []]
+
+// Question: 02
+// Declare and initialize a multidimensional array 
+// representing the following matrix:
+
+
+// var multidimensionalMatrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// console.log(multidimensionalMatrix [0]);
+// console.log(multidimensionalMatrix [1]);
+// console.log(multidimensionalMatrix [2]);
+
+// Question: 03
+// . Write a program to print numeric counting from 1 to 10.
+
+// for (let i = 1; i<=10; i++) {
+//     console.log(i);    
+// }
+
+// Question: 04
+
+// var number = prompt('Enter a number to show its table')
+// var length = prompt('Enter length of the number to show its table')
+
+// document.write("<h2> Multiplication table of " + number + "</h2>")
+// document.write("<p> length " + length + "</p>")
+
+// for(let i = 1; i <= 10; i++){
+//     document.write(number + "x" + i + "=" (number*i) + "</br>")
+// }
+
+// // Question: 05
+
+// var fruit = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (let i = 0; i < fruit.length; i++){
+//     console.log(fruit[i]);
+// }
+// console.log("Element at index " + i + " is ", fruit[i]);
+
