f86484654daf29be43fb6efc99ccb3b05f6d9ece
Muhammad Abdullah <abaloch0033@gmail.com>
2024-09-18 09:20:22 +0500

    updated

--- app.js
+++ app.js
@@ -37,7 +37,7 @@
 //     document.write(number + "x" + i + "=" (number*i) + "</br>")
 // }
 
-// // Question: 05
+// Question: 05
 
 // var fruit = ["apple", "banana", "mango", "orange", "strawberry"];
 
@@ -46,3 +46,65 @@
 // }
 // console.log("Element at index " + i + " is ", fruit[i]);
 
+// Question: 06
+
+// document.write("<h3>Counting:</h3>");
+// for (let i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<h3>Reverse counting:</h3>");
+// for (let i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<h3>Even:</h3>");
+// for (let i = 0; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<h3>Odd:</h3>");
+// for (let i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+
+// document.write("<h3>Series:</h3>");
+// for (let i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+
+// Question: 07
+
+// let bakeryItems = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// let userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?").toLowerCase();
+
+// let itemIndex = bakeryItems.indexOf(userInput);
+
+// if (itemIndex !== -1) {
+//     alert(userInput + " is available at index " + itemIndex + " in our bakery");
+// } else {
+//     alert("We are sorry. " + userInput + " is not available in our bakery");
+// }
+
+// Question: 08
+
+// let A = [24, 53, 78, 91, 12];
+// let largest = Math.max(...A);
+
+// console.log("Array items: " + A);
+// console.log("The largest number is " + largest);
+
+// Question: 09
+
+// let A = [24, 53, 78, 91, 12];
+// let smallest = Math.min(...A);
+
+// console.log("Array items: " + A);
+// console.log("The smallest number is " + smallest);
+
+// Question: 10
+
+// for (let i = 5; i <= 100; i += 5) {
+//     console.log(i);
+// }
